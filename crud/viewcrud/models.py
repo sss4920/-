03a2fee5857147e377d86f58a31d36f127fc1d95
soohyunkim <sss4920@likelion.org>
2019-11-26 16:42:57 +0900

    migrate

--- crud/viewcrud/models.py
+++ crud/viewcrud/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Blog(models.Model):
@@ -9,3 +10,11 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+class User(AbstractUser):
+    grade = models.IntegerField(null=True)
+    major = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=11)
+    student_id = models.CharField(max_length=20)
+    pass_or_not = models.BooleanField(null=True, blank=True)
+
+
--- crud/viewcrud/views.py
+++ crud/viewcrud/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from .models import Blog
+from .models import Blog,User
 from .forms import NewBlog
 # Create your views here.
 def welcome(request):
