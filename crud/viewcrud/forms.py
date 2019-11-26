047ac205bbbf497cefc2b81929dee401719235cb
soohyunkim <sss4920@likelion.org>
2019-11-26 15:27:49 +0900

    boolean

--- crud/viewcrud/forms.py
+++ crud/viewcrud/forms.py
@@ -4,4 +4,4 @@ from .models import Blog
 class NewBlog(forms.ModelForm):
     class Meta:
         model=Blog
-        fields = ['title', 'body']
+        fields = ['title', 'body', 'notice_or_not']
--- crud/viewcrud/models.py
+++ crud/viewcrud/models.py
@@ -5,6 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
+    notice_or_not = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
         return self.title
